8ea4f7da52f75e2a2a11268ef01b6a8cd5d75d69
zinosalaam1 <bolatitosalaam7@gmail.com>
2025-07-03 05:34:56 -0700

    initial

--- name.py
+++ name.py
@@ -1,1 +1,66 @@
-print('Hello World')
+# print('Hello World')
+
+
+# kolade = 5
+# print(kolade)
+
+# x = "Software"
+# print(x)
+
+# x,y,z = 1, 2, 3
+# print(z)
+# print(x)
+
+# x=y=z = 5
+# print(x)
+# print(y)
+# print(z)
+
+# fruits = ["apple", "banana", "cherry"]
+
+# print(fruits)
+
+list1 = ["apple", "banana", "cherry"]
+
+
+print(type(list1))
+
+
+mist1 = ("apple", "banana", "cherry")
+print(type(mist1))
+
+# monsur = float(35)
+# print(monsur)
+
+# z1 = complex(4, 7)
+# z2 = complex(2, 9)
+
+# print(z2)
+
+# c1 = 1 + 2j
+# c2 = 3 - 4j
+
+# print(c1+c2)
+
+# x = range(8)
+# print(x)
+
+# y = range(1, 10, 3)
+# for n in y:
+#   print(n)
+
+print(10+5)
+print(10*5)
+
+
+tion = {
+    "name": "monsur",
+    "location": "ibadan",
+    "year": 1761,
+    "year": 3239
+}
+
+print(tion)
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
